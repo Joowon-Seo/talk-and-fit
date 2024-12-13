eefb76e25fa9b129790cd04f9ee6a7ab337c3014
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2024-12-13 23:54:28 +0900

    init

---
+++ backend/src/test/java/com/ecommerce/talkandfit/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.talkandfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
